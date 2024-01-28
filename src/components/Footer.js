4af4444ec2a5a12c802029dee3b2b500cbb2a6e9
Ben <ben.oldham2003@gmail.com>
2024-01-28 13:06:04 +0000

    Add email signup form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Image from 'next/image';
 import CopyrightLabel from "@/components/CopyrightLabel";
+import EmailSignup from "@/components/EmailSignup";
 
 const Footer = () => {
     return (
@@ -13,9 +14,13 @@ const Footer = () => {
                     <p className="text-light font-light">Your future, simplified</p>
                 </div>
 
-                <div class="flex flex-row gap-5 text-light">
-                    <Image src={'/img-assets/app-store.svg'} width={0} height={0} style={{ width: 'auto', height: '40px' }} />
-                    <Image src={'/img-assets/play-store.svg'} width={0} height={0} style={{ width: 'auto', height: '40px' }} />
+                <div className="flex flex-row justify-between text-md mb-3">
+                    <div class="flex flex-row gap-5 text-light">
+                        <Image src={'/img-assets/app-store.svg'} width={0} height={0} style={{ width: 'auto', height: '40px' }} />
+                        <Image src={'/img-assets/play-store.svg'} width={0} height={0} style={{ width: 'auto', height: '40px' }} />
+                    </div>
+
+                    <EmailSignup />
                 </div>
 
                 <div class="flex flex-row gap-5 justify-center">
