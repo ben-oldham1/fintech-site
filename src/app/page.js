c0baf3797661efcdce685cc0f99bd7856bfb087d
Ben <ben.oldham2003@gmail.com>
2024-03-12 16:49:42 +0000

    Add new general info section

--- src/app/page.js
+++ src/app/page.js
@@ -3,17 +3,26 @@ import Image from "next/image";
 import StatCard from "@/ui/StatCard";
 import Button from "@/ui/Button";
 import FadeIn from "@/ui/FadeIn";
+import Accordion from "@/components/Accordion";
+import Modal from "@/components/Modal";
 
 export default function Home() {
+  const accordionItems = [
+    { title: 'Stocks', content: 'Invest in leading companies across various industries and sectors with ease.' },
+    { title: 'Crypto', content: 'Explore the world of digital currencies, including Bitcoin and Ethereum.' },
+    { title: 'Forex', content: 'Trade in the largest and most liquid market globally.' },
+    { title: 'ETFs', content: 'Access a diverse range of flexabile and stable investment options.' },
+  ];
+
   return (
     <>
       {/* THE HERO SECTION */}
       <section className='bg-gray-light'>
-        <div className='h-[calc(100vh-56px)] px-10 grid grid-cols-1 items-center gap-4 lg:grid-cols-5'>
+        <div className='h-[calc(100vh-56px)] px-32 grid grid-cols-1 items-center lg:gap-8 xl:gap-20 lg:grid-cols-5'>
           <div className="hidden lg:block">
-            <Image src={'/img-assets/iphone-mockup.png'} width={270} height={270} />
+            <Image src={'/img-assets/home-screenshot.png'} width={350} height={350} />
           </div>
-          <div className="md:col-span-3">
+          <div className="lg:col-span-4">
             <p className='text-xl font-light'><i className="bi bi-award"></i> iPhone app of the year (2024)</p>
             <h1 className='text-5xl my-2 leading-tight font-bold'>Take charge of your financial future with confidence and ease</h1>
             <p className="text-md font-light">14 day free trial</p>
@@ -69,6 +78,10 @@ export default function Home() {
       {/* GENERAL INFO SECTION TWO */}
       <section className='bg-light'>
         <div className='py-24 px-10 grid grid-cols-1 gap-32 md:grid-cols-2'>
+          <div className="mx-auto">
+            <img src={'/img-assets/home-screenshot.png'} width={150} height={150} />
+          </div>
+
           <div className="self-center">
             <FadeIn delay={0.2}>
               <h2 className="text-3xl mb-5 font-bold">See your portfolio at a glance</h2>
@@ -77,13 +90,40 @@ export default function Home() {
               <p className="text-2xl">Keep tabs on your investments with comprehensive portfolio tracking tools.</p>
             </FadeIn>
           </div>
-
-          <div className="mx-auto">
-            <img src={'/img-assets/iphone-mockup.png'} width={250} height={250} />
-          </div>
         </div>
 
       </section>
+
+      {/* GENERAL INFO SECTION THREE */}
+      <section className='bg-light'>
+        <div className='py-24 px-10 grid grid-cols-1 gap-32 md:grid-cols-2'>
+
+          <div className="self-center">
+            <FadeIn delay={0.2}>
+              <h2 className="text-3xl mb-5 font-bold">Invest in a wide range of assets</h2>
+            </FadeIn>
+            <FadeIn delay={0.4}>
+              <p className="text-2xl">Explore a wide range of investment opportunities, from traditional stocks to the latest cryptocurrencies.</p>
+            </FadeIn>
+
+            <FadeIn delay={0.2}>
+              <div className="mt-8">
+              <Button href="/download" variant="dark">
+                Start investing
+              </Button>
+              </div>
+            </FadeIn>
+          </div>
+
+          <div className="self-center">
+            <Accordion items={accordionItems} />
+          </div>
+
+        </div>
+
+      </section>
+
+      <Modal />
     </>
   )
 }
