71cf80f7ee9e38707ad1c3dc53a91c9e5ccc6060
Ben <ben.oldham2003@gmail.com>
2024-03-21 17:21:29 +0000

    Update article contents

--- src/app/learn/article/page.js
+++ src/app/learn/article/page.js
@@ -38,12 +38,12 @@ export default function Article() {
                             At its core, a blockchain is a decentralized and distributed ledger that records transactions across a network of computers. Unlike traditional centralized systems, such as banks, where a single authority oversees transactions, blockchain operates on a peer-to-peer network, making it more secure and transparent.
                         </p>
 
-                        <h2 className='mt-6 mb-2 text-2xl font-bold' id='What is Blockchain'>What is Blockchain?</h2>
+                        <h2 className='mt-6 mb-2 text-2xl font-bold' id='WhatisBlockchain'>What is Blockchain?</h2>
                         <p className='mb-2'>
                             Imagine a chain of blocks, where each block contains a list of transactions. Once a block reaches its capacity, a new block is added to the chain, creating a chronological and unalterable record of transactions. This chain is maintained by a network of computers, often referred to as nodes, each holding a copy of the entire blockchain.
                         </p>
 
-                        <h2 className='mt-6 mb-2 text-2xl font-bold' id='Decentralization and Trust'>Decentralization and Trust</h2>
+                        <h2 className='mt-6 mb-2 text-2xl font-bold' id='DecentralizationandTrust'>Decentralization and Trust</h2>
                         <p className='mb-2'>
                             One of the key features of blockchain is its decentralized nature. Instead of relying on a single entity, such as a bank or government, to verify and validate transactions, blockchain distributes this responsibility across a network of nodes. Each node has a copy of the entire blockchain, and for a transaction to be considered valid, it must be approved by a majority of nodes through a consensus mechanism.
                         </p>
@@ -57,7 +57,7 @@ export default function Article() {
                             </figcaption>
                         </figure>
 
-                        <h2 className='mt-6 mb-2 text-2xl font-bold' id='Cryptocurrencies and Blockchain'>Cryptocurrencies and Blockchain</h2>
+                        <h2 className='mt-6 mb-2 text-2xl font-bold' id='CryptocurrenciesandBlockchain'>Cryptocurrencies and Blockchain</h2>
                         <p className='mb-2'>
                             Cryptocurrencies are digital or virtual currencies that use cryptography for security and operate on blockchain technology. Bitcoin, the first and most well-known cryptocurrency, introduced the concept of a decentralized digital currency, enabling peer-to-peer transactions without the need for a central authority.
                         </p>
@@ -65,12 +65,12 @@ export default function Article() {
                             When someone initiates a cryptocurrency transaction, it is broadcasted to the network and grouped with other transactions into a block. Miners, participants in the network who use computational power to solve complex mathematical puzzles, compete to validate and add the block to the blockchain. The first miner to solve the puzzle is rewarded with newly minted cryptocurrency and transaction fees.
                         </p>
 
-                        <h2 className='mt-6 mb-2 text-2xl font-bold' id='Transparency and Immutability'>Transparency and Immutability</h2>
+                        <h2 className='mt-6 mb-2 text-2xl font-bold' id='TransparencyandImmutability'>Transparency and Immutability</h2>
                         <p className='mb-2'>
                             The transparency of blockchain is a crucial aspect that contributes to its trustworthiness. Anyone can view the entire transaction history on the blockchain, fostering accountability and reducing the risk of corruption. Once a block is added to the chain, it becomes virtually impossible to alter or delete past transactions due to the cryptographic link between blocks.
                         </p>
 
-                        <h2 className='mt-6 mb-2 text-2xl font-bold' id='Smart Contracts'>Smart Contracts</h2>
+                        <h2 className='mt-6 mb-2 text-2xl font-bold' id='SmartContracts'>Smart Contracts</h2>
                         <p className='mb-2'>
                             Blockchain technology also enables the creation and execution of smart contracts, self-executing contracts with the terms of the agreement directly written into code. Smart contracts automatically enforce and execute predefined rules, eliminating the need for intermediaries and streamlining various processes, such as legal agreements, supply chain management, and more.
                         </p>
@@ -88,21 +88,35 @@ export default function Article() {
                     <aside className='fixed'>
 
                         <div>
-                            <p className='text-xl font-bold'>Further reading</p>
-                            <ul>
-                                <li>Subtitle 1</li>
-                                <li>Subtitle 2</li>
-                                <li>Subtitle 3</li>
+                            <p className='text-xl font-bold'>Key takeaways</p>
+
+                            <ul className='list-disc space-y-4'>
+                                <li>Blockchain is a decentralized, distributed ledger technology that enhances security and transparency in digital transactions, underpinning the operation of cryptocurrencies.</li>
+                                <li>Eliminates the need for intermediaries by distributing transaction validation across a network, fostering trust through consensus mechanisms, cryptography, and immutability.</li>
+                                <li>Beyond supporting cryptocurrencies like Bitcoin, blockchain enables smart contracts and has the potential to revolutionize various sectors by automating and securing digital agreements and processes.</li>
                             </ul>
                         </div>
 
-                        <div className='mt-5'>
-                            <p className='text-xl font-bold'>Key takeaways</p>
-
+                        <div>
+                            <p className='mt-5 text-xl font-bold'>Further reading</p>
                             <ul>
-                                <li>Subtitle 1</li>
-                                <li>Subtitle 2</li>
-                                <li>Subtitle 3</li>
+                                <li>
+                                    <a className='hover:underline hover:text-orange' href='https://stellar.org/learn/blockchain-basics'>
+                                        Blockchain Basic | Stellar.org
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a className='hover:underline hover:text-orange' href='https://www.ibm.com/topics/blockchain'>
+                                        What is Blockchain | IBM
+                                    </a>
+                                </li>
+                                
+                                <li>
+                                    <a className='hover:underline hover:text-orange' href='https://www.investopedia.com/terms/b/blockchain.asp'>
+                                        What is a Blockchain | Investopedia
+                                    </a>
+                                </li>
                             </ul>
                         </div>
                     </aside>
