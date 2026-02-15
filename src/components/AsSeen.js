1ed580bf2843c7917c463cde7c433d4630a462a0
Ben <ben.oldham2003@gmail.com>
2026-02-15 16:21:29 +0000

    Update SVG to dark

--- src/components/AsSeen.js
+++ src/components/AsSeen.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Image from 'next/image';
 
-import Logo1 from "../../public/img-assets/asSeen/Logo1.svg";
-import Logo2 from "../../public/img-assets/asSeen/Logo2.svg";
-import Logo3 from "../../public/img-assets/asSeen/Logo3.svg";
+import Logo1 from "../../public/img-assets/asSeen/Logo1-dark.svg";
+import Logo2 from "../../public/img-assets/asSeen/Logo2-dark.svg";
+import Logo3 from "../../public/img-assets/asSeen/Logo3-dark.svg";
 import Logo4 from "../../public/img-assets/asSeen/Logo4.svg";
 import Logo5 from "../../public/img-assets/asSeen/Logo5.svg";
 
@@ -11,65 +11,39 @@ import FadeIn from "@/components/FadeIn";
 
 export default function AsSeen() {
     return (
-        <section className='bg-dark text-light'>
-            <div className='w-4/5 2xl:w-3/5 mx-auto py-8'>
-                <div className="pb-6 flex justify-center">
-                    <p className="text-2xl">
-                        As featured in
-                    </p>
+        <section>
+            <p className='uppercase text-sm text-center md:text-left mb-3'>
+                As seen in
+            </p>
+            <div className="flex flex-row gap-5 items-center justify-center md:justify-start z-0">
+                <div>
+                    <FadeIn delay={0.2}>
+                        <Image
+                            className='transition duration-200 hover:opacity-100 w-24 h-auto opacity-60'
+                            src={Logo3}
+                            alt="Financial Times logo"
+                        />
+                    </FadeIn>
                 </div>
-                <div className="grid grid-cols-2 gap-16 sm:grid-cols-3 lg:grid-cols-5 lg:gap-20 z-0">
 
-                    <div className='flex justify-center items-center'>
-                        <FadeIn delay={0.2}>
-                            <Image
-                                className='transform transition duration-200 hover:scale-105'
-                                src={Logo1}
-                                alt="Bloomberg logo"
-                            />
-                        </FadeIn>
-                    </div>
+                <div>
+                    <FadeIn delay={0.4}>
+                        <Image
+                            className='transition duration-200 hover:opacity-100 w-24 h-auto opacity-60'
+                            src={Logo1}
+                            alt="Bloomberg logo"
+                        />
+                    </FadeIn>
+                </div>
 
-                    <div className='flex justify-center items-center'>
-                        <FadeIn delay={0.4}>
-                            <Image
-                                className='transform transition duration-200 hover:scale-105'
-                                src={Logo2}
-                                alt="The Economist logo"
-                            />
-                        </FadeIn>
-                    </div>
-
-                    <div className='flex justify-center items-center'>
-                        <FadeIn delay={0.6}>
-                            <Image
-                                className='transform transition duration-200 hover:scale-105'
-                                src={Logo3}
-                                alt="Financial Times logo"
-                            />
-                        </FadeIn>
-                    </div>
-
-                    <div className='flex justify-center items-center'>
-                        <FadeIn delay={0.8}>
-                            <Image
-                                className='transform transition duration-200 hover:scale-105'
-                                src={Logo4}
-                                alt="Business Insider logo"
-                            />
-                        </FadeIn>
-                    </div>
-
-                    <div className='flex justify-center items-center'>
-                        <FadeIn delay={1.0}>
-                            <Image
-                                className='transform transition duration-200 hover:scale-105'
-                                src={Logo5}
-                                alt="The Guardian logo"
-                            />
-                        </FadeIn>
-                    </div>
-
+                <div>
+                    <FadeIn delay={0.6}>
+                        <Image
+                            className='transition duration-200 hover:opacity-100 w-24 h-auto opacity-60'
+                            src={Logo2}
+                            alt="The Economist logo"
+                        />
+                    </FadeIn>
                 </div>
             </div>
         </section>
