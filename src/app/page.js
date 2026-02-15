64cc076abc8682cb29284cc6c88eaea1c616e6cc
Ben <ben.oldham2003@gmail.com>
2026-02-15 16:09:45 +0000

    Cosmetic improvements

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 
 import StatCard from "@/components/StatCard";
+import Container from "@/components/Container";
 import Button from "@/components/Button";
 import FadeIn from "@/components/FadeIn";
 import Accordion from "@/components/Accordion";
 import AsSeen from "@/components/AsSeen";
+
+import { ShieldCheck, MessageCircleMore, Vault } from "lucide-react";
 
 export default function Home() {
   const accordionItems = [
@@ -17,33 +20,59 @@ export default function Home() {
   return (
     <>
       {/* THE HERO SECTION */}
-      <section className="2xl:w-3/5 mx-auto">
-        <div className='md:min-h-[calc(100vh-56px)] 2xl:min-h-[calc(60vh-56px)] px-8 gap-8 md:px-32 flex flex-col lg:flex-row py-16 lg:py-0 items-center'>
-          <div className="hidden lg:block">
-            <Image src={'/img-assets/home-screenshot.png'} width={350} height={350} />
-          </div>
-          <div className="lg:hidden w-1/2 mr-auto">
-            <Image src={'/img-assets/home-screenshot-half.png'} width={350} height={350} />
-          </div>
-          <div>
-            <p className='text-xl font-light'><i className="bi bi-award"></i> iPhone app of the year (2024)</p>
-            <h1 className='text-4xl leading-tight lg:text-5xl my-2 lg:leading-tight font-bold'>Take charge of your financial future with confidence and ease</h1>
-            <p className="text-md font-light">14 day free trial</p>
+      <Container className="mb-12 md:py-24">
+        <div className="flex flex-col-reverse md:flex-row gap-12">
+          <div className="basis-1/2">
+            <div className="border-2 border-dark rounded-full py-1 px-3 text-xs uppercase font-semibold w-fit mb-4">
+              <p>Trusted by 100,000+ users</p>
+            </div>
+            <h1 className='text-4xl leading-tight lg:text-6xl lg:leading-none font-bold mb-6'>The smarter way to <span className="text-orange">invest</span></h1>
+            <p className="text-xl mb-6">Experience a trading platform designed for clarity. Real-time insights, expert-curated portfolios, and security you can bank on.
 
-            <div className="mt-8 lg:mt-16 flex flex-row flex-wrap gap-x-2">
+            </p>
+            <div className="flex flex-row gap-2 mb-8 lg:mb-16">
               <Image src={'/img-assets/app-store.svg'} width={0} height={0} style={{ width: 'auto', height: '40px' }} />
               <Image src={'/img-assets/play-store.svg'} width={0} height={0} style={{ width: 'auto', height: '40px' }} />
             </div>
+
+            <div className="max-w-96">
+              {/* 'AS FEATURED' SECTION */}
+              <AsSeen />
+            </div>
+          </div>
+          <div className="basis-1/2 flex items-center justify-center">
+            <Image src={'/img-assets/herographic.png'} className="h-72 w-auto object-contain" width={1200} height={1200} />
           </div>
         </div>
-      </section>
 
-      {/* 'AS FEATURED' SECTION */}
-      <AsSeen />
+        <div className="bg-dark text-light rounded-md flex flex-col md:flex-row gap-4 items-center justify-between p-5 mt-12 md:mt-32">
+          <div className="flex flex-row gap-2">
+            <ShieldCheck />
+            <p className="font-semibold">
+              Fully FCA regulated
+            </p>
+          </div>
+          <hr className="border-light md:hidden opacity-20 w-full" />
+          <div className="flex flex-row gap-2">
+            <MessageCircleMore />
+            <p className="font-semibold">
+              24/7 human support
+            </p>
+          </div>
+          <hr className="border-light md:hidden opacity-20 w-full" />
+          <div className="flex flex-row gap-2">
+            <Vault />
+            <p className="font-semibold">
+              Bank-level security
+            </p>
+          </div>
+        </div>
+      </Container>
+
 
       {/* GENERAL INFO SECTION ONE */}
-      <section className='w-4/5 2xl:w-3/5 mx-auto bg-light'>
-        <div className='py-16 md:py-32 grid grid-cols-1 gap-16 md:gap-32 md:grid-cols-2'>
+      <Container className="pb-16 md:pb-24">
+        <div className='grid grid-cols-1 gap-16 md:gap-32 md:grid-cols-2'>
           <div>
             <FadeIn delay={0.2}>
               <p className="text-3xl mb-8">
@@ -79,10 +108,11 @@ export default function Home() {
           </div>
 
         </div>
-      </section>
+      </Container>
+
 
       {/* GENERAL INFO SECTION TWO */}
-      <section className='w-4/5 2xl:w-3/5 mx-auto bg-light'>
+      <Container className="pb-16 md:pb-24">
         <div className='py-16 md:py-24 grid grid-cols-1 gap-16 md:gap-32 md:grid-cols-5'>
           <div className="md:col-span-2 mx-auto">
             <img className="z-10" src={'/img-assets/home-screenshot-circle.png'} width={250} height={150} />
@@ -98,10 +128,10 @@ export default function Home() {
           </div>
         </div>
 
-      </section>
+      </Container>
 
       {/* GENERAL INFO SECTION THREE */}
-      <section className='w-4/5 2xl:w-3/5 mx-auto bg-light mb-20'>
+      <Container className="pb-16 md:pb-24">
         <div className='py-16 md:py-24 grid grid-cols-1 gap-16 md:gap-32 md:grid-cols-2'>
 
           <div className="self-center">
@@ -126,8 +156,8 @@ export default function Home() {
           </div>
 
         </div>
+      </Container>
 
-      </section>
     </>
   )
 }
